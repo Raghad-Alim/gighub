02e950051d99d495a50126f36becc928760cdc2a
norah <nouarif4@gmail.com>
2024-09-05 13:44:07 +0300

    initial commit with flutter app

---
+++ README.md
@@ -0,0 +1,4 @@
+<<<<<<< HEAD
+# Gighub
+
+this is part of the SWE444 course in KSU 
---
+++ android/app/src/main/kotlin/com/example/gighub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gighub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
